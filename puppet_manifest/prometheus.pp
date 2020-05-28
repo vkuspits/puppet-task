48652b676187503cf6e48a7c778aaee7ed280696
vkuspits@mirantis.com <vkuspits@mirantis.com>
2020-05-28 17:07:59 +0400

    Add openstack exporter

--- puppet_manifest/prometheus.pp
+++ puppet_manifest/prometheus.pp
@@ -15,6 +15,32 @@ class { 'prometheus::server':
         }
       ],
     },
+    {
+      'job_name'        => 'node-exporter',
+      'scrape_interval' => '30s',
+      'scrape_timeout'  => '30s',
+      'static_configs'  => [
+        {
+          'targets' => ['localhost:9100'],
+          'labels'  => {'alias' => 'Node'}
+        }
+      ],
+    },
+    {
+      'job_name'        => 'openstack-exporter',
+      'scrape_interval' => '30s',
+      'scrape_timeout'  => '30s',
+      'openstack_sd_configs'  => [
+        {
+          'region'             => ['Default'],
+          'identity_endpoint'  => ['localhost:5000/v3'],
+          'username'           => ['admin'],
+          'domain_name'        => ['admin'],
+          'password'           => ['admin123'],
+          'role'               => ['instance'],
+        }
+      ],
+    },
   ],
 }
 class { 'prometheus::node_exporter':
